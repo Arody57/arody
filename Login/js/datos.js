2c8b5b79b666a5f3200037176c75d0cb8b102d4c
AxelMarcos <34458277+AxelMarcos@users.noreply.github.com>
2018-09-07 08:15:31 -0600

    Add files via upload

--- Login/js/datos.js
+++ Login/js/datos.js
@@ -1,4 +1,7 @@
+var imagen;
 $(document).ready(function () {
+    $('#btnIniciar').hide();
+    // Seguridad para Saber si ha Iniciado Seccion
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             console.log('Conetado');
@@ -7,22 +10,53 @@ $(document).ready(function () {
             location.assign('login.html');
         }
     });
-        
+
+    $("#imagen").change(function () {
+        var descriptor = new FileReader();
+        descriptor.readAsDataURL(this.files[0]);
+
+        descriptor.onloadend = function () {
+            imagen = descriptor.result;
+            $("#previsualizacion").attr("src", imagen);
+        };
+    });
+
     $('#btnDatos').click(function () {
         var nombre = $('#nombre').val();
-        var user = firebase.auth().currentUser;
 
-        user.updateProfile({
-            displayName: nombre
-        }).then(function(){
+        if (nombre != "") {
+            var user = firebase.auth().currentUser;
+            user.updateProfile({
 
-            location.assign("index.html");
-            
-        }).catch(function (error) {
-            alert("Error" + error);
-            
-        });
+                displayName: nombre
+
+            }).then(GuardarDatos).catch(function (error) {
+                alert("Error" + error);
+            });
+        } else {
+            M.toast({
+                html: 'Insgrese un nombre de Usuario'
+            });
+        }
     });
-    
-    
+
+    $('#btnIniciar').click(function(){
+        location.assign('inicio.html');
+    });
 });
+
+function GuardarDatos() {
+    if (!imagen) {
+        imagen = "https://publicdomainvectors.org/photos/1389952697.png";
+    }
+    var user = firebase.auth().currentUser;
+
+    firebase.database().ref('Usuario/' + user.uid).set({
+        uid: user.uid,
+        Correo: user.email,
+        Nombre: user.displayName,
+        Foto: imagen
+    });
+    $('#btnIniciar').show();
+    $('#btnDatos').hide();
+}
