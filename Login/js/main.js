d2cd0babcaaca26230ee0c16bfdd2c9814df3ede
AxelMarcos <34458277+AxelMarcos@users.noreply.github.com>
2018-09-14 14:52:01 -0600

    Add files via upload

--- Login/js/main.js
+++ Login/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
             var user = firebase.auth().currentUser;
 
             // Se conecta a ala base de datos y Busca los valores del usuario conectado
-            firebase.database().ref("Usuario/" + user.uid)
+            firebase.database().ref("Usuario/" + user.uid + "/Cuenta")
                 .once("value").then(VerDatos);
 
             // Dibujar los valore en la paginaVerDatos
@@ -20,6 +20,12 @@ $(document).ready(function () {
             // la variable 'logeado' se va a dibujar en el ID nav-mobile
             $(logueado).appendTo('#nav-mobile');
 
+
+            firebase.database().ref("Usuario/" + user.uid + "/Dispositivos")
+                .once("value").then(VerDispositivos);
+
+            
+
             // Funcion cuando se haga click en el boton salir
             $(btnLogout).click(desconectar);
         } else {
@@ -27,16 +33,16 @@ $(document).ready(function () {
             location.assign('index.html');
         }
     });
-
-
     // Materialize para el menu lateral izquierdo 
     // lo que hace es esconder el menu cuando la pantalla este pequeño
     $('.sidenav').sidenav();
+
+
+    
 });
 
 // Funcion para dibujar los datos en la pagina
 // esta funcion se usa en la linea 12
-
 function VerDatos(snapshot) {
     var nombre = (snapshot.val() && snapshot.val().Nombre);
     var foto = (snapshot.val() && snapshot.val().Foto);
@@ -49,6 +55,17 @@ function VerDatos(snapshot) {
     $(MostrarDatos).appendTo('#Mostrardatos');
 
 }
+
+function VerDispositivos(snapshot){
+    var nombre = (snapshot.val() && snapshot.val().Nombre);
+    var serial = (snapshot.val() && snapshot.val().Serial);
+    var asig = (snapshot.val() && snapshot.val().Asignacion);
+
+    var dispo = '<tr><td>'+ nombre +'</td><td>'+ serial +'</td><td>'+ asig +'</td></tr>';
+
+    $(dispo).appendTo('#Dispositivos');
+}
+
 
 // Funcon para cerrar session
 // esta funcion se usa en la linea 24
